b9ca1a598547b2f70d0791f405016c3374ce2871
Nikhil <nikhilchacko1999@gmail.com>
2022-06-02 16:35:27 +0530

    a new view is created with 3 employee urls

--- empmgtsys/employee/urls.py
+++ empmgtsys/employee/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from employee import views
 urlpatterns = [
 
-    path('index', views.index)
+    path('index', views.index),     #**
+    path("login", views.login),	#**
+    path("signup",views.registration)	#**
 ]
-
--- empmgtsys/employee/views.py
+++ empmgtsys/employee/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-# Create your views here.
 
 def index(request):
     return HttpResponse("<h1> hello world</h1>")
+
+def login(request):
+    return HttpResponse ("<h1> login </h1>")
+
+def registration(request):
+    return HttpResponse("<h1> Welcome to registration </h1>")
