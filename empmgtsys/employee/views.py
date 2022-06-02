f71535d83f6edfd13051e000030949e3f76de40a
Nikhil <nikhilchacko1999@gmail.com>
2022-06-02 21:20:38 +0530

    templates were created for /signup,/login and /index urls

--- empmgtsys/employee/views.py
+++ empmgtsys/employee/views.py
@@ -1,10 +1,13 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("<h1> hello world</h1>")
 
+    return render(request,'index.html')
 def login(request):
-    return HttpResponse ("<h1> login </h1>")
+    print(',.,.,.,.,.,.,.,..',request.method) #show the method in terminal
+    print(',.,.,.,.,.,.,.,..', request)  #shows the request in the terminal
+    return render(request, 'login.html')
 
 def registration(request):
-    return HttpResponse("<h1> Welcome to registration </h1>")
+    return render(request, 'register.html')
