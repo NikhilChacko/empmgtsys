66535ccb524b385b73d3871d4b3862b20265f4be
Nikhil <nikhilchacko1999@gmail.com>
2022-06-02 22:49:10 +0530

    Class based view is created

--- empmgtsys/employee/urls.py
+++ empmgtsys/employee/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from employee import views
 urlpatterns = [
 
-    path('index', views.index),     #**
-    path("login", views.login),	#**
-    path("signup",views.registration)	#**
+    path('index', views.Indexview.as_view()),     #**
+    path("login", views.Loginview.as_view()),	#**
+    path("signup",views.Signupview.as_view())	#**
 ]
--- empmgtsys/employee/views.py
+++ empmgtsys/employee/views.py
@@ -1,13 +1,37 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.generic import View
 
-def index(request):
+# def index(request):
+#
+#     return render(request,'index.html')
+# def login(request):
+#     print(',.,.,.,.,.,.,.,..',request.method) #show the method in terminal
+#     print(',.,.,.,.,.,.,.,..', request)  #shows the request in the terminal
+#     return render(request, 'login.html')
+#
+# def registration(request):
+#     return render(request, 'register.html')
 
-    return render(request,'index.html')
-def login(request):
-    print(',.,.,.,.,.,.,.,..',request.method) #show the method in terminal
-    print(',.,.,.,.,.,.,.,..', request)  #shows the request in the terminal
-    return render(request, 'login.html')
+class Indexview(View):
 
-def registration(request):
-    return render(request, 'register.html')
+    def get(self, request):
+        return render(request, "index.html")
+    def post(selfrequest):
+        pass
+
+class Loginview(View):
+
+    def get(self, request):
+        return render(request, "login.html")
+    def post(selfrequest):
+        pass
+
+
+class Signupview(View):
+
+    def get(self, request):
+        return render(request, "register.html")
+
+    def post(selfrequest):
+        pass
