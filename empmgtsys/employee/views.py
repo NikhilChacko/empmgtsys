1424a56bc2f2398151031033949ba8072ae4a56e
Nikhil <nikhilchacko1999@gmail.com>
2022-06-04 02:19:15 +0530

    post methos was implemented for forms

--- empmgtsys/employee/views.py
+++ empmgtsys/employee/views.py
@@ -17,15 +17,17 @@ class Indexview(View):
 
     def get(self, request):
         return render(request, "index.html")
-    def post(selfrequest):
-        pass
+    def post(self,request):
+        print("here in post")
 
 class Loginview(View):
 
     def get(self, request):
         return render(request, "login.html")
-    def post(selfrequest):
-        pass
+    def post(self,request):
+        print(request.POST["name"])
+        print(request.POST["pass"])
+        return render(request, "login.html")
 
 
 class Signupview(View):
@@ -33,5 +35,9 @@ class Signupview(View):
     def get(self, request):
         return render(request, "register.html")
 
-    def post(selfrequest):
-        pass
+    def post(self, request):
+        print("here in post")
+        print(request.POST["email"])
+        print(request.POST["psw"])
+        print(request.POST["psw-repeat"])
+        return render(request, "register.html")
